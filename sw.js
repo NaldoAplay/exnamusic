e3fd55644538d3b4cf9170ea0a0227182d11bc9e
Naldo <edinaldoandradeoficial@gmail.com>
2026-09-19 12:08:19 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
+const CACHE_NAME = 'exna-music-v3';
+const assetsToCache = [
+    'index.html',
+    'manifest.json',
+    'icon.png'
+];
+
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
-        caches.open('exna-music-v2').then((cache) => {
-            return cache.addAll([
-                'index.html',
-                'manifest.json',
-                'icon.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assetsToCache);
         })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
         })
     );
 });
